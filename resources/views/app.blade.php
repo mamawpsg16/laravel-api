3d2409b6c0ccd65c66d73e960e9928fc9b1b0bde
Kevin Mensah <48464499+mamawpsg16@users.noreply.github.com>
2024-06-14 17:21:29 +0800

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Laravel-Vue App</title>
+    <link href="{{ asset('dist/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app"></div>
+
+    <script src="{{ asset('dist/js/app.js') }}"></script>
+</body>
+</html>
